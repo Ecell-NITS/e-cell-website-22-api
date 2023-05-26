0126342d6801a628487feda306a0a7c4ee45e5a0
hereisadi <hereisadi@proton.me>
2023-05-26 16:21:11 +0530

    upd reply msg

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ app.post("/sendquery", async (req, res) => {
   const newUser = new UserModel2(user);
   await newUser.save();
   const email = user.email;
-  const subject = "Your query submitted🥳";
-  const text = `Thanks for reaching out to ecell.`;
+  const subject = "Your Sent Us a Message";
+  const text = `Hey there! You sent:\n\n\n\n>${user.message}\n\n\n\nWe'll get back to you as soon as we can! Hopefully with a useful answer.\n\n\n\nThanks,\nThe NITS ECELL Technical Team.`;
   sendEmail(email, subject, text);
   res.json(user);
 });
